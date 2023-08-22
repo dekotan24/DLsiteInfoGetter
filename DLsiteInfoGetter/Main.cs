364e0857aed216d9a044c3e0d49839d90d11f3e0
Ogura Deko <fanta51@live.jp>
2023-08-23 02:33:24 +0900

    v1.2

--- DLsiteInfoGetter/Main.cs
+++ DLsiteInfoGetter/Main.cs
@@ -1,214 +1,84 @@
-﻿using System;
+﻿using AngleSharp.Html.Parser;
+using System;
 using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
 
 namespace DLsiteInfoGetter
 {
-	public class Main
+	public class DLsiteInfo
 	{
-		// 共通変数
-		private bool foundFlg = true;
-		private string prodID = string.Empty;
-		private string prodTitle = string.Empty;
-		private string prodCircle = string.Empty;
-		private string prodType = string.Empty;
-		private string prodImage = string.Empty;
-		private string errMsg = string.Empty;
+		// プロパティ
+		public string ProductId { get; private set; } // 作品ID
+		public string Title { get; private set; } // 作品名
+		public string Circle { get; private set; } // サークル名
+		public string ProductType { get; private set; } // 作品種別
+		public string ImageUrl { get; private set; } // 作品画像のURL
 
-		// 共通関数
-		/// <summary>
-		/// DLsiteから作品情報を取得します。
-		/// </summary>
-		/// <param name="rawText">作品IDもしくはURL</param>
-		/// <param name="productID">作品ID</param>
-		/// <param name="title">作品名</param>
-		/// <param name="circle">サークル名</param>
-		/// <param name="productType">作品種別</param>
-		/// <param name="errorMessage">エラーメッセージ</param>
-		/// <returns>True：作品が存在する / False：作品が存在しない</returns>
-		public bool GetInfo(string rawText, out string productID, out string title, out string circle, out string productType, out string imageUrl, out string errorMessage)
+		// コンストラクタ
+		private DLsiteInfo(string productId, string title, string circle, string productType, string imageUrl)
 		{
-			string ansText = string.Empty;
-
-			if (!string.IsNullOrEmpty(rawText))
-			{
-				ansText = textAnalyze(rawText);
-
-				if (!string.IsNullOrEmpty(ansText))
-				{
-					searchExecute(ansText, rawText);
-				}
-				else
-				{
-					setErrMsg("切り出し結果が不正です。(E002)");
-				}
-			}
-			else
-			{
-				setErrMsg("URLが不正です。(E001)");
-			}
-
-			// 結果格納
-			productID = prodID;
-			title = prodTitle;
-			circle = prodCircle;
-			productType = prodType;
-			imageUrl = prodImage;
-			errorMessage = errMsg;
-
-			return foundFlg;
+			ProductId = productId;
+			Title = title;
+			Circle = circle;
+			ProductType = productType;
+			ImageUrl = imageUrl;
 		}
 
-		/// <summary>
-		/// 入力されたテキストを作品ID形式に整形します。
-		/// </summary>
-		/// <param name="rawText">生の値（URLもしくは作品ID形式）</param>
-		/// <returns>作品ID形式</returns>
-		private string textAnalyze(string rawText)
+		// 作品情報を取得するメソッド
+		public static DLsiteInfo GetInfo(string rawText)
 		{
-			string ansText = string.Empty;
-			int beginID = -1;
-			string productTypeID = string.Empty;
+			// 入力されたテキストを作品ID形式に整形する
+			string productId = TextAnalyze(rawText);
+
+			if (string.IsNullOrEmpty(productId))
+			{
+				throw new ArgumentException("入力されたテキストが作品ID形式ではありません。");
+			}
+
+			// 作品種別を判定する
+			string productType = productId.Substring(0, 2);
+
+			// 作品情報を取得するURLを構築する
+			string url = "https://www.dlsite.com/" + (productType == "RJ" ? "maniax" : "pro") + "/work/=/product_id/" + productId + ".html";
+
+			// 作品情報を取得する
+			using (WebClient wc = new WebClient())
+			{
+				wc.Encoding = Encoding.UTF8;
+				string html = wc.DownloadString(url);
+
+				// HTMLからタイトル、サークル名、画像URLを抽出する
+				var parser = new HtmlParser();
+				var doc = parser.ParseDocument(html);
+				var titleElement = doc.QuerySelector("#work_name");
+				var circleElement = doc.QuerySelector("span.maker_name > a:nth-child(1)");
+				var imageElement = doc.QuerySelector("meta[property='og:image']");
+				string title = titleElement.TextContent;
+				string circle = circleElement.TextContent;
+				string imageUrl = imageElement.GetAttribute("content");
+
+				// 作品情報のインスタンスを返す
+				return new DLsiteInfo(productId, title, circle, productType, imageUrl);
+			}
+		}
+
+		// 入力されたテキストを作品ID形式に整形するメソッド
+		private static string TextAnalyze(string rawText)
+		{
 			string upperText = rawText.ToUpper();
 
-			// RJ：一般向け作品のID。例: RJ123456
-			if (upperText.Contains("RJ"))
-			{
-				productTypeID = "RJ";
-			}
-			// VJ：18禁作品のID。例: VJ123456
-			else if (upperText.Contains("VJ"))
-			{
-				productTypeID = "VJ";
-			}
-			// RE：一般向け作品の体験版のID。例: RE123456
-			else if (upperText.Contains("RE"))
-			{
-				productTypeID = "RE";
-			}
-			// VE：18禁作品の体験版のID。例: VE123456
-			else if (upperText.Contains("VE"))
-			{
-				productTypeID = "VE";
-			}
-			// BJ：ブックマーク用のID。例: BJ123456
-			else if (upperText.Contains("BJ"))
-			{
-				productTypeID = "BJ";
-			}
-			// AJ：アフィリエイト用のID。例: AJ123456
-			else if (upperText.Contains("AJ"))
-			{
-				productTypeID = "AJ";
-			}
-			// 識別不可
-			else
-			{
-				productTypeID = "NA";
-			}
-
-			// 作品ID計算処理
-			beginID = upperText.IndexOf((productTypeID));
-			if (beginID < 0)
+			// URLから一番最初のRJもしくはVJなどの作品種別+数字（6桁以上）を抽出する
+			Match match = Regex.Match(upperText, @"(RJ|VJ|RE|VE|BJ|AJ)[0-9]{6,}");
+			if (!match.Success)
 			{
 				return string.Empty;
 			}
 
-			// 文字切り出し
-			ansText = upperText.Substring(beginID);
+			// 抽出した文字列を作品IDとして返す
+			string productId = match.Value;
 
-			// URLの可能性を考慮し、不要文字列を削除する
-			ansText = ansText.Replace("/", "").Replace(".HTML", "");
-
-			// 大文字化
-			ansText = ansText.ToUpper();
-
-			// (RJ|VJ|RE|VE|BJ|AJ)xxxxx形式かチェックする
-			if (!Convert.ToBoolean(new Regex("^(" + productTypeID + ")+[0-9]+$").IsMatch(ansText)))
-			{
-				setErrMsg("正規表現チェックでエラーが発生しました。文字列の切り出し結果が不正です。(E003)[" + ansText + "]");
-				return string.Empty;
-			}
-
-			// 結果反映
-			prodType = productTypeID;
-			prodID = ansText;
-
-			return ansText;
-		}
-
-		/// <summary>
-		/// 作品情報取得処理
-		/// </summary>
-		/// <param name="rawText">作品ID形式の値</param>
-		/// <param name="rawUrl">生の値</param>
-		private void searchExecute(string rawText, string rawUrl)
-		{
-			// イメージ作成用のURL構築
-			string url = string.Empty;
-			string str = Regex.Replace(rawText, @"[^0-9]", "");
-			int id = int.Parse(str);
-			string fullID = prodType;
-			int kuriage = (id / 1000) * 1000 + 1000;
-			string kuriage2 = kuriage.ToString().PadLeft(rawText.Length - prodType.Length, '0');
-
-			fullID = prodType + id.ToString().PadLeft(rawText.Length - prodType.Length, '0');
-
-			prodImage = "https://img.dlsite.jp/modpub/images2/work/" + (prodType.Equals("RJ") ? "doujin" : "professional") + "/" + prodType + kuriage2 + "/" + fullID + "_img_main.jpg";
-
-			// 作品名取得
-			if (!rawUrl.Contains("dlsite.com"))
-			{
-				rawUrl = "https://www.dlsite.com/" + (prodType.Equals("RJ") ? "maniax" : "pro") + "/work/=/product_id/" + rawUrl.ToUpper();
-			}
-			if (!rawUrl.Contains(".html"))
-			{
-				rawUrl += ".html";
-			}
-
-			try
-			{
-				ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
-				WebClient wc = new WebClient();
-				System.IO.Stream st = wc.OpenRead(rawUrl);
-
-				var parser = new AngleSharp.Html.Parser.HtmlParser();
-				var doc = parser.ParseDocument(st);
-
-				var title2 = doc.QuerySelector("#work_name");
-				var circle2 = doc.QuerySelector("span.maker_name > a:nth-child(1)");
-				string title = title2.TextContent;
-				string circle = circle2.TextContent;
-				prodTitle = title;
-				prodCircle = circle;
-			}
-			catch (Exception ex)
-			{
-				setErrMsg("作品詳細取得中にエラーが発生しました。(E004)[" + ex.Message + "]");
-			}
-
-			return;
-		}
-
-		/// <summary>
-		/// エラーメッセージをセットします。
-		/// </summary>
-		/// <param name="message">エラーメッセージ</param>
-		private void setErrMsg(string message)
-		{
-			StringBuilder sb = new StringBuilder();
-			if (!string.IsNullOrEmpty(errMsg))
-			{
-				sb.Append(errMsg).Append("\n").Append(message);
-			}
-			else
-			{
-				sb.Append(message);
-			}
-
-			errMsg = sb.ToString();
-			foundFlg = false;
+			return productId;
 		}
 	}
 }
